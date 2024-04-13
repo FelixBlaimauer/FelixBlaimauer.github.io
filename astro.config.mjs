bfb58b3001c45a56f22fd62490a4aea6fac25d18
Felix Blaimauer <blaimauer.felix@gmail.com>
2024-04-13 18:36:36 +0200

    Optimization Integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import solidJs from '@astrojs/solid-js';
 import mdx from '@astrojs/mdx';
-import dynamicImportVars from '@rollup/plugin-dynamic-import-vars';
 import icon from 'astro-icon';
+import playformInline from '@playform/inline';
+import playformCompress from '@playform/compress';
+
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
@@ -25,6 +28,17 @@ export default defineConfig({
       include: {
         heroicons: ['*'],
         mdi: ['linkedin', 'github'],
+      },
+    }),
+    playformInline(),
+    playformCompress(),
+    sitemap({
+      i18n: {
+        defaultLocale: 'en',
+        locales: {
+          en: 'en-US',
+          de: 'de-DE',
+        },
       },
     }),
   ],
